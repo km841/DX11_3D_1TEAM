adb1fe49b9c1e06c8c838638eefcaef6a1852124
sangy227 <sangy227@gmail.com>
2023-08-11 22:28:58 +0900

    카메라 스크립트 상하좌우 반전 풀기
    카메라 스크립트 상하좌우 반전 풀기

--- OurGame/Client/DiningColliderCheckMap.cpp
+++ OurGame/Client/DiningColliderCheckMap.cpp
@@ -1,44 +1,134 @@
 #include "pch.h"
 #include "DiningColliderCheckMap.h"
+#include "Engine.h"
 
-DiningColliderCheckMap::DiningColliderCheckMap()
-	: Map(MapType::DiningColliderCheckMap)
-{
-}
+/* Resource */
+#include "MeshData.h"
+#include "Material.h"
+#include "Mesh.h"
 
-DiningColliderCheckMap::~DiningColliderCheckMap()
-{
-}
+/* Manager */
+#include "PrefabManager.h"
+#include "EventManager.h"
+#include "Factory.h"
+#include "CollisionManager.h"
+#include "Input.h"
+#include "SceneManager.h"
+#include "Resources.h"
 
-void DiningColliderCheckMap::Initialize()
-{
-	Map::Initialize();
-}
+/* GameObject */
+#include "GameObject.h"
+#include "Player.h"
+#include "Ground.h"
+#include "DecoObject.h"
+#include "WallObject.h"
 
-void DiningColliderCheckMap::Update()
-{
-	Map::Update();
-}
+/* Component */
+#include "Collider.h"
+#include "RigidBody.h"
+#include "MeshRenderer.h"
+#include "Transform.h"
+#include "Camera.h"
+#include "Light.h"
+#include "ParticleSystem.h"
 
-void DiningColliderCheckMap::FixedUpdate()
-{
-	Map::FixedUpdate();
-}
+/* Script */
 
-void DiningColliderCheckMap::FinalUpdate()
-{
-	Map::FinalUpdate();
-}
 
-void DiningColliderCheckMap::Render()
-{
-	Map::Render();
-}
+/* Event */
+#include "SceneChangeEvent.h"
 
-void DiningColliderCheckMap::Enter()
-{
-}
 
-void DiningColliderCheckMap::Exit()
+namespace hm
 {
+	DiningColliderCheckMap::DiningColliderCheckMap()
+		: Map(MapType::DiningColliderCheckMap)
+	{
+	}
+
+	DiningColliderCheckMap::~DiningColliderCheckMap()
+	{
+	}
+
+	void DiningColliderCheckMap::Initialize()
+	{
+		Map::Initialize();
+	}
+
+	void DiningColliderCheckMap::Update()
+	{
+		Map::Update();
+	}
+
+	void DiningColliderCheckMap::FixedUpdate()
+	{
+		Map::FixedUpdate();
+	}
+
+	void DiningColliderCheckMap::FinalUpdate()
+	{
+		Map::FinalUpdate();
+	}
+
+	void DiningColliderCheckMap::Render()
+	{
+		Map::Render();
+	}
+
+	void DiningColliderCheckMap::Enter()
+	{
+		
+		//WallObject µµ ¿÷¿Ω
+
+		// NormalBase
+		{
+			DecoObject* pNormalBase = Factory::CreateObject<DecoObject>(Vec3(0.f, 0.f, 0.f), L"Deferred", L"..\\Resources\\FBX\\Map\\Dungeon\\DiningColliderCheckMap\\Diningroom.fbx");
+
+			pNormalBase->GetTransform()->SetScale(Vec3(50.f, 50.f, 50.f));
+			//pNormalBase->GetTransform()->SetPositionExcludingColliders(Vec3(-12.f, -18.5f, 0.f));
+
+			AddGameObject(pNormalBase);
+		}
+
+		// Wall
+		/*for (int y = 0; y < 4; ++y)
+		{
+			for (int x = 0; x < 12; ++x)
+			{
+				DecoObject* pDecoObject = Factory::CreateObject<DecoObject>(Vec3(0.f, 0.f, 0.f), L"Deferred", L"..\\Resources\\FBX\\Map\\Dungeon\\GrandmaBossWall1.fbx");
+				pDecoObject->GetRigidBody()->RemoveGravity();
+
+				float radian = (225.f + (x * 16.f)) * XM_PI / 180.f;
+				float radius = 13.f;
+
+				pDecoObject->GetTransform()->SetScale(Vec3(10.f, 10.f, 10.f));
+				pDecoObject->GetTransform()->SetRotation(Vec3(0.f, 50.f + x * 16.f, 0.f));
+
+				pDecoObject->GetTransform()->SetPosition(
+					Vec3(radius * cosf(radian) + radius * sinf(radian),
+						-20.f + y * 10.f,
+						(radius * cosf(radian) - radius * sinf(radian)) + 10.f));
+				AddGameObject(pDecoObject);
+			}
+		}*/
+
+		// Create Desk
+		/*{
+			PhysicsInfo info = {};
+			info.eActorType = ActorType::Dynamic;
+			info.size = Vec3(10.f, 3.f, 1.f);
+
+			Player* pGameObject = Factory::CreateObjectHasPhysical<Player>(Vec3(0.f, 0.f, 0.f), info, L"Deferred", L"..\\Resources\\FBX\\Map\\Hall\\Desk.fbx");
+			pGameObject->GetTransform()->SetScale(Vec3(10.f, 10.f, 10.f));
+			pGameObject->GetTransform()->SetRotationExcludingColliders(Vec3(0.f, -45.f, 0.f));
+			pGameObject->GetTransform()->SetPositionExcludingColliders(Vec3(0.f, 0.f, -1.f));
+
+			AddGameObject(pGameObject);
+			GET_SINGLE(PrefabManager)->AddPrefab(L"Test2", pGameObject);
+		}*/
+	}
+
+	void DiningColliderCheckMap::Exit()
+	{
+	}
 }
