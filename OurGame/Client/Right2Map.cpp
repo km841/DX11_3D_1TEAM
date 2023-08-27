0607f8b58eb8832e2c42724c92d6f32093675d1b
hmkim <mooneyes90@naver.com>
2023-08-27 17:37:25 +0900

    Work Right2Map 90% complete
    Work Right2Map 90% complete

--- OurGame/Client/Right2Map.cpp
+++ OurGame/Client/Right2Map.cpp
@@ -87,18 +87,6 @@ namespace hm
 		GET_SINGLE(CollisionManager)->SetCollisionGroup(LayerType::Player, LayerType::Ground);
 		GET_SINGLE(CollisionManager)->SetCollisionGroup(LayerType::Player, LayerType::WallObject);
 
-		// Banker
-		{
-			DecoObject* pBanker = Factory::CreateObject<DecoObject>(Vec3(0.f, 10.f, 0.f), L"Deferred", L"..\\Resources\\FBX\\Map\\Dungeon\\Right2Map\\Banker.fbx");
-			pBanker->GetTransform()->SetScale(Vec3(1.f, 1.f, 1.f));
-			pBanker->GetTransform()->SetRotation(Vec3(0.89f, 124.7f, 225.2f));
-			pBanker->AddComponent(new TestAnimationScript);
-			//pBanker->AddComponent(new PlacementScript);
-
-			SetGizmoTarget(pBanker);
-			AddGameObject(pBanker);
-		}
-
 		// Ground
 		{
 			PhysicsInfo physicsInfo;
@@ -149,24 +137,7 @@ namespace hm
 
 		}
 
-
-		// Point Light
-		//{
-		//	DecoObject* pGameObject = new DecoObject();
-		//	Transform* pTransform = pGameObject->AddComponent(new Transform);
-		//	pTransform->SetPosition(Vec3(0.f, 5.f, 0.f));
-		//	pTransform->SetScale(Vec3(10.f, 10.f, 10.f));
-		//	Light* pLight = pGameObject->AddComponent(new Light);
-		//	pLight->SetDiffuse(Vec3(1.f, 1.f, 1.f));
-		//	pLight->SetAmbient(Vec3(0.f, 0.f, 0.f));
-		//	pLight->SetLightRange(80.f);
-		//	pLight->SetLightType(LightType::PointLight);
-		//	AddGameObject(pGameObject);
-		//}
-
-
-
-		// 벽쪽 2단 선반
+		// 좌측 벽쪽 2단 선반
 		{
 			for (int i = 0; i < 4; ++i)
 			{
@@ -176,12 +147,58 @@ namespace hm
 				AddGameObject(pTwoTierShelf);
 			}
 
+			// 선반 안쪽 항아리
+
+			for (int i = 0; i < 6; ++i)
+			{
+				DecoObject* pMansionSpicePot = Factory::CreateObject<DecoObject>(Vec3(-12.4f + i * 2.6f, 3.2f, 16.1f), L"Deferred", L"..\\Resources\\FBX\\Map\\Dungeon\\Right2Map\\MansionSpicePot.fbx");
+				pMansionSpicePot->GetTransform()->SetScale(Vec3(2.3f, 2.3f, 2.3f));
+				
+				AddGameObject(pMansionSpicePot);
+			}
+
 			// 혼자 가로로 삐져나온 선반 (식탁보로 덮혀질 예정)
 			{
 				DecoObject* pTwoTierShelf = Factory::CreateObject<DecoObject>(Vec3(-5.6f, 2.8f, 10.2f), L"Deferred", L"..\\Resources\\FBX\\Map\\Dungeon\\Right2Map\\TwoTierShelf.fbx");
 				pTwoTierShelf->GetTransform()->SetScale(Vec3(8.5f, 8.5f, 8.5f));
 				pTwoTierShelf->GetTransform()->SetRotation(AXIS_Y, 90.f);
 				AddGameObject(pTwoTierShelf);
+
+				// 선반 안쪽 서랍들
+				{
+					{
+						DecoObject* pEmptyBox = Factory::CreateObject<DecoObject>(Vec3(-5.8f, 3.5f, 12.2f), L"Deferred", L"..\\Resources\\FBX\\Map\\Dungeon\\Right2Map\\EmptyBox.fbx");
+						pEmptyBox->GetTransform()->SetScale(Vec3(3.5f, 3.5f, 3.5f));
+						pEmptyBox->GetTransform()->SetRotation(Vec3(0.f, -90.f, 0.f));
+						
+						AddGameObject(pEmptyBox);
+					}
+
+					{
+						DecoObject* pEmptyBox = Factory::CreateObject<DecoObject>(Vec3(-5.3f, 3.5f, 10.1f), L"Deferred", L"..\\Resources\\FBX\\Map\\Dungeon\\Right2Map\\EmptyBox.fbx");
+						pEmptyBox->GetTransform()->SetScale(Vec3(3.5f, 3.5f, 3.5f));
+						pEmptyBox->GetTransform()->SetRotation(Vec3(0.f, -90.f, 0.f));
+
+						AddGameObject(pEmptyBox);
+					}
+
+					{
+						DecoObject* pEmptyBox = Factory::CreateObject<DecoObject>(Vec3(-5.8f, 3.5f, 8.f), L"Deferred", L"..\\Resources\\FBX\\Map\\Dungeon\\Right2Map\\EmptyBox.fbx");
+						pEmptyBox->GetTransform()->SetScale(Vec3(3.5f, 3.5f, 3.5f));
+						pEmptyBox->GetTransform()->SetRotation(Vec3(0.f, -90.f, 0.f));
+						
+						AddGameObject(pEmptyBox);
+					}
+				}
+
+				// 깨지는 항아리
+				{
+					DecoObject* pPotHeal = Factory::CreateObject<DecoObject>(Vec3(-9.3f, 7.3f, 16.1f), L"Deferred", L"..\\Resources\\FBX\\Map\\Dungeon\\Right2Map\\POT_HEAL_Generic.fbx");
+					pPotHeal->GetTransform()->SetScale(Vec3(3.f, 3.f, 3.f));
+					pPotHeal->GetTransform()->SetRotation(Vec3(0.f, 90.f, 0.f));
+					SetGizmoTarget(pPotHeal);
+					AddGameObject(pPotHeal);
+				}
 			}
 
 			// 식탁보
@@ -189,10 +206,6 @@ namespace hm
 				DecoObject* pTableCloth = Factory::CreateObject<DecoObject>(Vec3(21.7f, 8.6f, 6.5f), L"Deferred_CullNone", L"..\\Resources\\FBX\\Map\\Dungeon\\Right2Map\\tableCloth.fbx");
 				pTableCloth->GetTransform()->SetScale(Vec3(15.4f, 10.f, 23.3f));
 				pTableCloth->GetTransform()->SetRotation(Vec3(-180.f, 0.f, 180.f));
-				//pTableCloth->AddComponent(new PlacementScript);
-				//pKey->AddComponent(new RotateKeyScript);
-				pTableCloth->GetMeshRenderer()->GetMaterial()->SetBloom(true);
-				pTableCloth->GetMeshRenderer()->GetMaterial()->SetBloomColor(Vec4(1.f, 1.f, 0.3f, 1.f));
 				AddGameObject(pTableCloth);
 			}
 		}
@@ -223,6 +236,40 @@ namespace hm
 				pTwoTierShelf->GetTransform()->SetScale(Vec3(8.5f, 8.5f, 8.5f));
 
 				AddGameObject(pTwoTierShelf);
+			}
+
+			// 선반 안쪽 서랍들
+			{
+				{
+					DecoObject* pEmptyBox = Factory::CreateObject<DecoObject>(Vec3(5.5f, 3.5f, -2.8f), L"Deferred", L"..\\Resources\\FBX\\Map\\Dungeon\\Right2Map\\EmptyBox.fbx");
+					pEmptyBox->GetTransform()->SetScale(Vec3(3.5f, 3.5f, 3.5f));
+					AddGameObject(pEmptyBox);
+				}
+
+				{
+					DecoObject* pEmptyBox = Factory::CreateObject<DecoObject>(Vec3(7.6f, 3.5f, -2.2f), L"Deferred", L"..\\Resources\\FBX\\Map\\Dungeon\\Right2Map\\EmptyBox.fbx");
+					pEmptyBox->GetTransform()->SetScale(Vec3(3.5f, 3.5f, 3.5f));
+					
+					AddGameObject(pEmptyBox);
+				}
+
+				{
+					DecoObject* pEmptyBox = Factory::CreateObject<DecoObject>(Vec3(9.7f, 3.5f, -2.8f), L"Deferred", L"..\\Resources\\FBX\\Map\\Dungeon\\Right2Map\\EmptyBox.fbx");
+					pEmptyBox->GetTransform()->SetScale(Vec3(3.5f, 3.5f, 3.5f));
+					//SetGizmoTarget(pEmptyBox);
+					AddGameObject(pEmptyBox);
+				}
+			}
+
+			// 사다리
+			{
+				DecoObject* pLadder = Factory::CreateObject<DecoObject>(Vec3(5.f, 2.43f, -1.1f), L"Deferred", L"..\\Resources\\FBX\\Map\\Dungeon\\Right2Map\\Ladder.fbx");
+				pLadder->GetTransform()->SetScale(Vec3(6.5f, 6.5f, 6.5f));
+
+				//SetGizmoTarget(pLadder);
+
+				AddGameObject(pLadder);
+
 			}
 		}
 
@@ -257,15 +304,47 @@ namespace hm
 		{
 			DecoObject* pChair = Factory::CreateObject<DecoObject>(Vec3(16.2f, 3.5f, 7.3f), L"Deferred", L"..\\Resources\\FBX\\Map\\Dungeon\\Right2Map\\Chair.fbx");
 
-
+			
 			pChair->GetTransform()->SetScale(Vec3(7.f, 7.f, 7.f));
 			pChair->GetTransform()->SetRotation(Vec3(0.f, 18.f, 0.f));
 			AddGameObject(pChair);
 		}
 
+		// 항아리 삼총사
+		{
+			// 깨지는 항아리
+			{
+				DecoObject* pPotHeal = Factory::CreateObject<DecoObject>(Vec3(12.8f, 1.18f, 4.7f), L"Deferred", L"..\\Resources\\FBX\\Map\\Dungeon\\Right2Map\\POT_HEAL_Generic.fbx");
+				pPotHeal->GetTransform()->SetScale(Vec3(3.f, 3.f, 3.f));
+				pPotHeal->GetTransform()->SetRotation(Vec3(0.f, 90.f, 0.f));
+				AddGameObject(pPotHeal);
+			}
+
+			// 깨지는 항아리
+			{
+				DecoObject* pPotHeal = Factory::CreateObject<DecoObject>(Vec3(11.6f, 1.18f, 6.8f), L"Deferred", L"..\\Resources\\FBX\\Map\\Dungeon\\Right2Map\\POT_HEAL_Generic.fbx");
+				pPotHeal->GetTransform()->SetScale(Vec3(3.f, 3.f, 3.f));
+				pPotHeal->GetTransform()->SetRotation(Vec3(0.f, 115.5f, 0.f));
+
+				//SetGizmoTarget(pPotHeal);
+				AddGameObject(pPotHeal);
+			}
+
+			// 깨지는 항아리
+			{
+				DecoObject* pPotHeal = Factory::CreateObject<DecoObject>(Vec3(14.f, 1.18f, 6.8f), L"Deferred", L"..\\Resources\\FBX\\Map\\Dungeon\\Right2Map\\POT_HEAL_Generic.fbx");
+				pPotHeal->GetTransform()->SetScale(Vec3(3.f, 3.f, 3.f));
+				pPotHeal->GetTransform()->SetRotation(Vec3(0.f, 36.7f, 0.f));
+				
+				AddGameObject(pPotHeal);
+			}
+		}
+
+
+
 		// 우측 벽에 붙은 선반들
 		{
-			// 2단 선반
+			// 2단 선반 (안쪽, 바깥쪽)
 			for (int i = 0; i < 2; ++i)
 			{
 				DecoObject* pTwoTierShelf = Factory::CreateObject<DecoObject>(Vec3(i == 0 ? 13.9f : -8.2f, 2.8f, -16.5f), L"Deferred", L"..\\Resources\\FBX\\Map\\Dungeon\\Right2Map\\TwoTierShelf.fbx");
@@ -273,14 +352,96 @@ namespace hm
 				AddGameObject(pTwoTierShelf);
 			}
 
-			// 2단 선반과 이어지는 한 칸짜리 선반
+			// 선반 안에 그릇들
+			for (int i = 0; i < 2; ++i)
 			{
-				DecoObject* pTwoTierCornerFoot = Factory::CreateObject<DecoObject>(Vec3(20.1f, 2.7f, -16.5f), L"Deferred", L"..\\Resources\\FBX\\Map\\Dungeon\\Right2Map\\TwoTierSquareCornerFoot.fbx");
-				pTwoTierCornerFoot->GetTransform()->SetScale(Vec3(6.5f, 6.3f, 6.0f));
+				for (int j = 0; j < 2; ++j)
+				{
+					if (i == 1 && j == 0)
+						continue;
+
+					DecoObject* pPlates = Factory::CreateObject<DecoObject>(Vec3(15.5f + j * -3.5f, 2.8f + i * 0.8f, -16.5f), L"Deferred", L"..\\Resources\\FBX\\Map\\Dungeon\\Right2Map\\Plates.fbx");
+					pPlates->GetTransform()->SetScale(Vec3(3.f, 3.f, 3.f));
+					
+					AddGameObject(pPlates);
+				}
+
+			}
+
+			// 선반 안에 서랍들
+			{
+				{
+					DecoObject* pEmptyBox = Factory::CreateObject<DecoObject>(Vec3(-6.f, 3.5f, -16.6f), L"Deferred", L"..\\Resources\\FBX\\Map\\Dungeon\\Right2Map\\EmptyBox.fbx");
+					pEmptyBox->GetTransform()->SetScale(Vec3(3.5f, 3.5f, 3.5f));
+
+					AddGameObject(pEmptyBox);
+				}
+
+				{
+					DecoObject* pEmptyBox = Factory::CreateObject<DecoObject>(Vec3(-8.1f, 3.5f, -15.8f), L"Deferred", L"..\\Resources\\FBX\\Map\\Dungeon\\Right2Map\\EmptyBox.fbx");
+					pEmptyBox->GetTransform()->SetScale(Vec3(3.5f, 3.5f, 3.5f));
+					//SetGizmoTarget(pEmptyBox);
+					AddGameObject(pEmptyBox);
+				}
+
+				{
+					DecoObject* pEmptyBox = Factory::CreateObject<DecoObject>(Vec3(-10.2f, 3.5f, -16.6f), L"Deferred", L"..\\Resources\\FBX\\Map\\Dungeon\\Right2Map\\EmptyBox.fbx");
+					pEmptyBox->GetTransform()->SetScale(Vec3(3.5f, 3.5f, 3.5f));
+					//SetGizmoTarget(pEmptyBox);
+					AddGameObject(pEmptyBox);
+				}
+			}
+
+			// 2단 선반과 이어지는 한 칸짜리 선반 (바깥쪽)
+			{
+				DecoObject* pTwoTierCornerFoot = Factory::CreateObject<DecoObject>(Vec3(20.1f, 2.88f, -16.5f), L"Deferred", L"..\\Resources\\FBX\\Map\\Dungeon\\Right2Map\\TwoTierSquareCornerFoot.fbx");
+				pTwoTierCornerFoot->GetTransform()->SetScale(Vec3(6.5f, 5.86f, 6.0f));
 				pTwoTierCornerFoot->GetTransform()->SetRotation(AXIS_Y, 180.f);
 
-
 				AddGameObject(pTwoTierCornerFoot);
+			}
+
+			// 2단 선반과 이어지는 한 칸짜리 선반 (안쪽)
+			{
+				DecoObject* pTwoTierCornerFoot = Factory::CreateObject<DecoObject>(Vec3(-14.4f, 2.88f, -16.5f), L"Deferred", L"..\\Resources\\FBX\\Map\\Dungeon\\Right2Map\\TwoTierSquareCornerFoot.fbx");
+				pTwoTierCornerFoot->GetTransform()->SetScale(Vec3(6.5f, 5.86f, 6.0f));
+				AddGameObject(pTwoTierCornerFoot);
+			}
+
+			// 깨지는 항아리
+			{
+				DecoObject* pPotHeal = Factory::CreateObject<DecoObject>(Vec3(-9.8f, 6.88f, -16.6f), L"Deferred", L"..\\Resources\\FBX\\Map\\Dungeon\\Right2Map\\POT_HEAL_Generic.fbx");
+				pPotHeal->GetTransform()->SetScale(Vec3(3.f, 3.f, 3.f));
+				pPotHeal->GetTransform()->SetRotation(Vec3(0.f, 90.f, 0.f));
+				AddGameObject(pPotHeal);
+			}
+
+			// 화분
+			{
+				//MansionPlantPot
+				DecoObject* pMansionPlantPot = Factory::CreateObject<DecoObject>(Vec3(20.1f, 8.1f, -16.5f), L"Deferred", L"..\\Resources\\FBX\\Map\\Dungeon\\Right2Map\\MansionPlantPot.fbx");
+				pMansionPlantPot->GetTransform()->SetScale(Vec3(5.f, 5.f, 5.f));
+				
+				AddGameObject(pMansionPlantPot);
+			}
+
+			// 사다리
+			{
+				DecoObject* pLadder = Factory::CreateObject<DecoObject>(Vec3(-14.f, 2.63f, -14.7f), L"Deferred", L"..\\Resources\\FBX\\Map\\Dungeon\\Right2Map\\Ladder.fbx");
+				pLadder->GetTransform()->SetScale(Vec3(6.5f, 6.5f, 6.5f));
+
+				AddGameObject(pLadder);
+				
+			}
+
+			// 바깥쪽 책 쌓여있는 곳
+			{
+				DecoObject* pBookStack = Factory::CreateObject<DecoObject>(Vec3(23.5f, 3.5f, -15.4f), L"Deferred", L"..\\Resources\\FBX\\Map\\Dungeon\\Right2Map\\BookStack.fbx");
+				pBookStack->GetTransform()->SetScale(Vec3(7.2f, 7.2f, 7.2f));
+				pBookStack->GetTransform()->SetRotation(AXIS_Y, 180.f);
+				
+
+				AddGameObject(pBookStack);
 			}
 
 			// 화롯불
@@ -318,8 +479,6 @@ namespace hm
 			{
 				DecoObject* pFirelamp = Factory::CreateObject<DecoObject>(Vec3(-5.9f, 7.4f, -2.8f), L"Deferred", L"..\\Resources\\FBX\\Map\\Dungeon\\Right2Map\\GRANDMA_FireLamp.fbx");
 
-				pFirelamp->GetMeshRenderer()->GetMaterial()->SetBloom(true);
-				pFirelamp->GetMeshRenderer()->GetMaterial()->SetBloomPower(1.5f);
 				pFirelamp->GetTransform()->SetScale(Vec3(3.f, 3.f, 3.f));
 				//pTwoTierFootAbs->GetTransform()->SetRotation(AXIS_Y, 180.f);
 				AddGameObject(pFirelamp);
@@ -430,18 +589,45 @@ namespace hm
 			AddGameObject(pKey);
 		}
 
+		// 샹들리에
+		{
+			DecoObject* pChandelier = Factory::CreateObject<DecoObject>(Vec3(5.f, 37.6f, 8.8f), L"Deferred", L"..\\Resources\\FBX\\Map\\Dungeon\\Right2Map\\ChandelierWithChain.fbx");
+			pChandelier->GetTransform()->SetScale(Vec3(50.f, 50.f, 50.f));
+			pChandelier->GetMeshRenderer()->GetMaterial()->SetBloom(true, 13, 0);
+			pChandelier->GetMeshRenderer()->GetMaterial()->SetBloomPower(1.5f, 13, 0);
+			pChandelier->GetMeshRenderer()->GetMaterial()->SetBloomColor(Vec4(1.0f, 1.0f, 0.4f, 1.f), 13, 0);
 
+			pChandelier->GetMeshRenderer()->GetMaterial()->SetBloom(true, 14, 0);
+			pChandelier->GetMeshRenderer()->GetMaterial()->SetBloomPower(1.5f, 14, 0);
+			pChandelier->GetMeshRenderer()->GetMaterial()->SetBloomColor(Vec4(1.0f, 1.0f, 0.4f, 1.f), 14, 0);
 
-		// Bloom Texture
-		//{
-		//	UI* pUI = Factory::CreateObject<UI>(Vec3(-500.f, 200.f, 0.f), L"Forward", L"");
-		//	pUI->GetRigidBody()->RemoveGravity();
-		//	pUI->GetTransform()->SetScale(Vec3(300.f, 300.f, 1.f));
-		//	shared_ptr<Texture> pTexture = GET_SINGLE(Resources)->Get<Texture>(L"BlurTempTex");
+			pChandelier->GetMeshRenderer()->GetMaterial()->SetBloom(true, 15, 0);
+			pChandelier->GetMeshRenderer()->GetMaterial()->SetBloomPower(1.5f, 15, 0);
+			pChandelier->GetMeshRenderer()->GetMaterial()->SetBloomColor(Vec4(1.0f, 1.0f, 0.4f, 1.f), 15, 0);
 
-		//	pUI->GetMeshRenderer()->GetMaterial()->SetTexture(0, pTexture);
-		//	AddGameObject(pUI);
-		//}
+			pChandelier->GetMeshRenderer()->GetMaterial()->SetBloom(true, 16, 0);
+			pChandelier->GetMeshRenderer()->GetMaterial()->SetBloomPower(1.5f, 16, 0);
+			pChandelier->GetMeshRenderer()->GetMaterial()->SetBloomColor(Vec4(1.0f, 1.0f, 0.4f, 1.f), 16, 0);
+
+			pChandelier->GetMeshRenderer()->GetMaterial()->SetBloom(true, 17, 0);
+			pChandelier->GetMeshRenderer()->GetMaterial()->SetBloomPower(1.5f, 17, 0);
+			pChandelier->GetMeshRenderer()->GetMaterial()->SetBloomColor(Vec4(1.0f, 1.0f, 0.4f, 1.f), 17, 0);
+
+			pChandelier->GetMeshRenderer()->GetMaterial()->SetBloom(true, 18, 0);
+			pChandelier->GetMeshRenderer()->GetMaterial()->SetBloomPower(1.5f, 18, 0);
+			pChandelier->GetMeshRenderer()->GetMaterial()->SetBloomColor(Vec4(1.0f, 1.0f, 0.4f, 1.f), 18, 0);
+
+			pChandelier->GetMeshRenderer()->GetMaterial()->SetBloom(true, 19, 0);
+			pChandelier->GetMeshRenderer()->GetMaterial()->SetBloomPower(1.5f, 19, 0);
+			pChandelier->GetMeshRenderer()->GetMaterial()->SetBloomColor(Vec4(1.0f, 1.0f, 0.4f, 1.f), 19, 0);
+
+			pChandelier->GetMeshRenderer()->GetMaterial()->SetBloom(true, 20, 0);
+			pChandelier->GetMeshRenderer()->GetMaterial()->SetBloomPower(1.5f, 20, 0);
+			pChandelier->GetMeshRenderer()->GetMaterial()->SetBloomColor(Vec4(1.0f, 1.0f, 0.4f, 1.f), 20, 0);
+
+			//SetGizmoTarget(pChandelier);
+			AddGameObject(pChandelier);
+		}
 	}
 
 	void Right2Map::Exit()
