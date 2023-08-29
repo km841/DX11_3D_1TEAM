f77474e18c0058d342a3574ee2f819f66f148edb
sangy227 <sangy227@gmail.com>
2023-08-29 18:57:18 +0900

    EntranceHallMap 작업중

--- OurGame/Client/EntranceHallMap.cpp
+++ OurGame/Client/EntranceHallMap.cpp
@@ -242,7 +242,7 @@ void EntranceHallMap::Enter()
 	//Deferred_CullNone == 머테리얼 카메라 시점 짤림 없애는 버전 _원본 그대로
 
 	//shared_ptr<Texture> pTexture = GET_SINGLE(Resources)->Load<Texture>(L"ObjectTexture", L"..\\Resources\\FBX\\Map\\Dungeon\\R_EntranceHallMap\\EntranceHall.fbm\\grandmaWallPaperTexture.png");
-	//Object->GetMeshRenderer()->GetMaterial()->SetTexture(0, pTexture);
+	//Object->GetMeshRenderer()->GetMaterial()->SetTexture(0, pTexture); == 텍스쳐 번호 설정
 
 	//Object->AddComponent(new PlacementScript); == 키보드로 움직이는 오브젝트 이동 스크립트
 	//SetGizmoTarget(Object); == 유니티 방식으로 마우스로 움직이는 오브젝트 이동 함수
@@ -254,7 +254,7 @@ void EntranceHallMap::Enter()
 		pEntranceHall->GetTransform()->SetScale(Vec3(50.f, 50.f, 50.f));
 
 		shared_ptr<Texture> pTexture = GET_SINGLE(Resources)->Load<Texture>(L"EntranceHallTexture", L"..\\Resources\\FBX\\Map\\Dungeon\\R_EntranceHallMap\\EntranceHall.fbm\\grandmaWallPaperTexture.png");
-		pEntranceHall->GetMeshRenderer()->GetMaterial()->SetTexture(0, pTexture); // 텍스쳐 강제로 적용시키는법
+		pEntranceHall->GetMeshRenderer()->GetMaterial()->SetTexture(0, pTexture); // 텍스쳐 강제로 적용시키는법aaaaaaaaaaaaa
 
 		pEntranceHall->GetMeshRenderer()->GetMaterial()->SetUVTiling(Vec2(0.03f, 0.03f));
 
@@ -266,11 +266,18 @@ void EntranceHallMap::Enter()
 		DecoObject* pEntranceHallFloor = Factory::CreateObject<DecoObject>(Vec3(0.1f, -9.7f, 4.2f), L"Deferred", L"..\\Resources\\FBX\\Map\\Dungeon\\R_EntranceHallMap\\EntranceHallFloor.fbx");
 		pEntranceHallFloor->GetTransform()->SetScale(Vec3(58.5f, 1.f, 58.5f));
 
-		
-
 		pEntranceHallFloor->GetMeshRenderer()->GetMaterial()->SetUVTiling(Vec2(0.05f, 0.05f));
 		
 		AddGameObject(pEntranceHallFloor);
+	}
+
+	//바닥 프레임 - wallPanel_Merged
+	{
+		DecoObject* pwallPanel_Merged = Factory::CreateObject<DecoObject>(Vec3(-0.4f, -10.6f, 4.5f), L"Deferred", L"..\\Resources\\FBX\\Map\\Dungeon\\R_EntranceHallMap\\wallPanel_Merged.fbx");
+		pwallPanel_Merged->GetTransform()->SetRotation(Vec3(0.f, 180.f, 0.f));
+		pwallPanel_Merged->GetTransform()->SetScale(Vec3(41.4f, 41.4f, 41.4f));
+
+		AddGameObject(pwallPanel_Merged);
 	}
 
 #pragma region "기둥"
@@ -287,12 +294,97 @@ void EntranceHallMap::Enter()
 		DecoObject* pColumnFull = Factory::CreateObject<DecoObject>(Vec3(11.7f, 0.85f, -14.5f), L"Deferred", L"..\\Resources\\FBX\\Map\\Dungeon\\R_EntranceHallMap\\ColumnFull.fbx");
 		pColumnFull->GetTransform()->SetRotation(Vec3(0.f, 90.f, 0.f));
 		pColumnFull->GetTransform()->SetScale(Vec3(30.f, 21.f, 30.f));
-		SetGizmoTarget(pColumnFull);
+	
+		AddGameObject(pColumnFull);
+	}
+
+	{
+		//세번째 기둥 - ColumnFull
+		DecoObject* pColumnFull = Factory::CreateObject<DecoObject>(Vec3(-11.3f, 0.85f, -14.8f), L"Deferred", L"..\\Resources\\FBX\\Map\\Dungeon\\R_EntranceHallMap\\ColumnFull.fbx");
+		pColumnFull->GetTransform()->SetRotation(Vec3(0.f, 180.f, 0.f));
+		pColumnFull->GetTransform()->SetScale(Vec3(30.f, 21.f, 30.f));
+		
 		AddGameObject(pColumnFull);
 	}
 #pragma endregion
 
+#pragma region "가로줄"
+	//가로줄 - bannisterTop
+	{
+		DecoObject* pbannisterTop = Factory::CreateObject<DecoObject>(Vec3(12.4f, 9.8f, 5.2f), L"Deferred", L"..\\Resources\\FBX\\Map\\Dungeon\\R_EntranceHallMap\\bannisterTop.fbx");
+		pbannisterTop->GetTransform()->SetRotation(Vec3(0.f, 90.f, 0.f));
+		pbannisterTop->GetTransform()->SetScale(Vec3(40.f, 130.f, 40.f));
+		
 
+		AddGameObject(pbannisterTop);
+	}
+
+	//가로줄 - bannisterTop
+	{
+		DecoObject* pbannisterTop = Factory::CreateObject<DecoObject>(Vec3(0.4f, 9.8f, -14.8f), L"Deferred", L"..\\Resources\\FBX\\Map\\Dungeon\\R_EntranceHallMap\\bannisterTop.fbx");
+		pbannisterTop->GetTransform()->SetRotation(Vec3(0.f, 0.f, 0.f));
+		pbannisterTop->GetTransform()->SetScale(Vec3(25.f, 130.f, 40.f));
+		
+
+		AddGameObject(pbannisterTop);
+	}
+#pragma endregion
+
+#pragma region "벽패널"
+	//벽패널 1 - WallPanel	
+	{
+		DecoObject* pWallPanel = Factory::CreateObject<DecoObject>(Vec3(11.8f, -6.6f, 17.5f), L"Deferred", L"..\\Resources\\FBX\\Map\\Dungeon\\R_EntranceHallMap\\WallPanel.fbx");
+		pWallPanel->GetTransform()->SetRotation(Vec3(0.f, 90.f, 0.f));
+		pWallPanel->GetTransform()->SetScale(Vec3(10.f, 10.f, 10.f));
+		
+		AddGameObject(pWallPanel);
+	}
+
+	//벽패널 2 - WallPanel	
+	{
+		DecoObject* pWallPanel = Factory::CreateObject<DecoObject>(Vec3(11.8f, -6.6f, 7.5f), L"Deferred", L"..\\Resources\\FBX\\Map\\Dungeon\\R_EntranceHallMap\\WallPanel.fbx");
+		pWallPanel->GetTransform()->SetRotation(Vec3(0.f, 90.f, 0.f));
+		pWallPanel->GetTransform()->SetScale(Vec3(10.f, 10.f, 10.f));
+		
+		AddGameObject(pWallPanel);
+	}
+
+	//벽패널 3 - WallPanel	
+	{
+		DecoObject* pWallPanel = Factory::CreateObject<DecoObject>(Vec3(11.8f, -6.6f, -2.5f), L"Deferred", L"..\\Resources\\FBX\\Map\\Dungeon\\R_EntranceHallMap\\WallPanel.fbx");
+		pWallPanel->GetTransform()->SetRotation(Vec3(0.f, 90.f, 0.f));
+		pWallPanel->GetTransform()->SetScale(Vec3(10.f, 10.f, 10.f));
+		
+		AddGameObject(pWallPanel);
+	}
+
+	//벽패널 4 - WallPanel	
+	{
+		DecoObject* pWallPanel = Factory::CreateObject<DecoObject>(Vec3(11.8f, -6.6f, -12.5f), L"Deferred", L"..\\Resources\\FBX\\Map\\Dungeon\\R_EntranceHallMap\\WallPanel.fbx");
+		pWallPanel->GetTransform()->SetRotation(Vec3(0.f, 90.f, 0.f));
+		pWallPanel->GetTransform()->SetScale(Vec3(10.f, 10.f, 10.f));
+
+		AddGameObject(pWallPanel);
+	}
+
+	//벽패널 세로칸 - WallPanel_shot
+	{
+		DecoObject* pWallPanel_shot = Factory::CreateObject<DecoObject>(Vec3(9.3f, -8.5f, -15.f), L"Deferred", L"..\\Resources\\FBX\\Map\\Dungeon\\R_EntranceHallMap\\WallPanel_shot.fbx");
+		pWallPanel_shot->GetTransform()->SetRotation(Vec3(0.f, 180.f, 0.f));
+		pWallPanel_shot->GetTransform()->SetScale(Vec3(10.f, 10.f, 10.f));
+	
+		AddGameObject(pWallPanel_shot);
+	}
+
+	//벽패널 세로칸 2  - WallPanel_shot
+	{
+		DecoObject* pWallPanel_shot = Factory::CreateObject<DecoObject>(Vec3(-7.3f, -8.5f, -15.f), L"Deferred", L"..\\Resources\\FBX\\Map\\Dungeon\\R_EntranceHallMap\\WallPanel_shot.fbx");
+		pWallPanel_shot->GetTransform()->SetRotation(Vec3(0.f, 180.f, 0.f));
+		pWallPanel_shot->GetTransform()->SetScale(Vec3(6.f, 10.f, 10.f));
+		
+		AddGameObject(pWallPanel_shot);
+	}
+#pragma endregion
 
 
 
