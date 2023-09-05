ea8155778b40567c4817619ea7a7578a07c86e38
hmkim <mooneyes90@naver.com>
2023-09-05 21:48:31 +0900

    Fix typo
    - 오탈자 수정

--- OurGame/Client/TitleScene.cpp
+++ OurGame/Client/TitleScene.cpp
@@ -63,7 +63,7 @@ namespace hm
 		// - GrandmaBossMap
 		if (IS_DOWN(KeyType::P))
 		{
-			GET_SINGLE(EventManager)->PushSceneChangeEvent(MapType::HallColliderCheckMap);
+			GET_SINGLE(EventManager)->PushSceneChangeEvent(MapType::RightSecretPassageMap);
 		}
 
 		// ÁöÇü
