fb09692c0ba7f86cd9811c2d248e98300c8d59bc
KailCrane <64236907+KailCrane@users.noreply.github.com>
2023-09-11 04:54:51 +0900

    콜라이더 삽입

--- OurGame/Client/MainOfficeMap.h
+++ OurGame/Client/MainOfficeMap.h
@@ -15,7 +15,7 @@ namespace yj {
 		virtual void FixedUpdate();
 		virtual void FinalUpdate();
 		virtual void Render();
-
+			
 		virtual void Enter();
 		virtual void Exit();
 		GameObject* Target;
